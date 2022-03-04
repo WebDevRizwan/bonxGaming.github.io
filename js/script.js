78b69cd0064243f68235cfa8c9fbe9d59d8df04d
RizwanRangrej <RizwanRangrej@users.noreply.github.com>
2022-03-05 00:40:22 +0530

    bonxGAming
    bonx Gaming website

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$("#slider").owlCarousel({
+    items:1,
+    nav:true,
+    loop:true,
+    dots:false,
+    // autoplay:true,
+    // autoplayTimeout:3000,
+    animateOut: 'slideOutDown',
+    animateIn: 'flipInX',
+    margin:10,
+    navText:["<img src='img/navigation-arrow2.webp'>" ,"<img src='img/navigation-arrow1.webp'>"]
+});
+
+
+$("#testimoniaL-slider").owlCarousel({
+    items:1,
+    loop:true,
+    dots:false,
+    nav:true,
+    navText:["<img src='img/navigation-arrow2.webp'>" ,"<img src='img/navigation-arrow1.webp'>"]
+});
